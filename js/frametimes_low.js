23f908c09d0d4e064cb396c41d4353872b7f67c4
Arman <arman1880@mail.ru>
2023-09-30 02:35:17 +0400

    push

--- js/frametimes_low.js
+++ js/frametimes_low.js
@@ -66,5 +66,6 @@ console.log($f1);
   console.log(nestedSubarray);
 ////
 const FEWFEW = [1,2,3,4,5];
+const FEWpush = FEWFEW.push([6,7,8]);
 const deleteFEW = FEWFEW.pop();
-console.log(FEWFEW[3]);
+console.log(FEWFEW[7]);
