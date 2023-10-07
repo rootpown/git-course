5d7c56e265e5ac014bc49135c31ffd0035d895c6
Arman <arman1880@mail.ru>
2023-10-07 23:07:27 +0400

    reverse

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-function testLogicalAnd(val) {
-  // Only change code below this line
-if ( val > 20 && val > 15 || val < 11 && val < 10) {
-  return "true";
-}
-  return "false";
-}
-console.log(testLogicalAnd(11));
-
-
+function chainToSwitch(val) {
+	let answer = "";
+	// Only change code below this line
   
+	if (val === "bob") {
+	  answer = "Marley";
+	} else if (val === 42) {
+	  answer = "The Answer";
+	} else if (val === 1) {
+	  answer = "There is no #1";
+	} else if (val === 99) {
+	  answer = "Missed me by this much!";
+	} else if (val === 7) {
+	  answer = "Ate Nine";
+	}
+  
+	// Only change code above this line
+	return answer;
+  }
+  
+  chainToSwitch(7);
